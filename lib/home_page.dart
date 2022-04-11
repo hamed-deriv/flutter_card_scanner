a8d7a20a3c1a134bf407eb783f2d0e4776854fab
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-04-11 09:31:52 +0800

    implement basic logic

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:credit_card_scanner/credit_card_scanner.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key, required this.title}) : super(key: key);
 
@@ -10,13 +12,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  CardDetails? _cardDetails;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: const Center(),
+      body: Center(
+        child: Column(
+          children: [
+            const Text('Scan a credit card'),
+            const SizedBox(height: 16),
+            Text('Card Number: ${_cardDetails?.cardNumber}'),
+            Text('Expiry Date : ${_cardDetails?.expiryDate}'),
+            Text('Holder Name: ${_cardDetails?.cardHolderName}'),
+            Text('Card Issuer: ${_cardDetails?.cardIssuer}'),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Scan',
+        child: const Icon(Icons.camera_alt),
+        onPressed: () async {
+          _cardDetails = await CardScanner.scanCard();
+
+          setState(() {});
+        },
+      ),
     );
   }
 }
